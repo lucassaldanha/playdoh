41fefd404f576f02ccb9c92463fece39bcd2b119
Lucas Saldanha <lucas.saldanha@m4u.com.br>
2015-07-03 12:27:40 -0300

    New objects for tests

---
+++ src/test/java/com/lsoftware/playdoh/objects/ObjectWithCollection.java
@@ -0,0 +1,18 @@
+package com.lsoftware.playdoh.objects;
+
+import java.util.Collection;
+
+public class ObjectWithCollection {
+
+    private Collection collection;
+
+    public ObjectWithCollection() {}
+
+    public Collection getCollection() {
+        return collection;
+    }
+
+    public void setCollection(Collection collection) {
+        this.collection = collection;
+    }
+}
---
+++ src/test/java/com/lsoftware/playdoh/objects/ObjectWithIntegerStringMap.java
@@ -0,0 +1,18 @@
+package com.lsoftware.playdoh.objects;
+
+import java.util.Map;
+
+public class ObjectWithIntegerStringMap {
+
+    private Map<Integer, String> map;
+
+    public ObjectWithIntegerStringMap() {}
+
+    public Map<Integer, String> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<Integer, String> map) {
+        this.map = map;
+    }
+}
